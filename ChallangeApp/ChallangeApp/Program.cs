80dab016c99700e26ea6a4a8aa386d439a626f2c
TutSSon <michal.tutak86@gmail.com>
2023-07-24 13:05:01 +0200

    Praca domowa dzień 3

--- ChallangeApp/ChallangeApp/Program.cs
+++ ChallangeApp/ChallangeApp/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string name = "Ewa";
+bool woman = true;
+int age = 33;
+
+if (name == "Ewa" && age == 33) 
+{
+    Console.WriteLine("Ewa, wiek 33 lata");
+}
+
+else if (woman == true && age < 30) 
+{
+    Console.WriteLine("Kobieta poniżej 30");
+}
+
+else if (woman == false && age < 18) 
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
+
+
+
